7753556941eb610aa9b0eb781b4d612f46e1c553
iFew <chitpong@gmail.com>
2020-04-10 13:59:27 +0700

    increase quality and low latency config

--- js/publish.js
+++ js/publish.js
@@ -409,7 +409,7 @@ function wsConnect(url) {
 
 		var mediaRecorderCoder = 'video/webm;codecs=h264';
 		
-		mediaStream = localVideo.captureStream(24); // 24 FPS
+		mediaStream = localVideo.captureStream(30); // 30 FPS
 		if(!MediaRecorder.isTypeSupported('video/webm;codecs=h264')){
 			if(MediaRecorder.isTypeSupported('video/webm; codecs=hevc')){
 				mediaRecorderCoder = 'video/webm; codecs=hevc';
@@ -422,12 +422,12 @@ function wsConnect(url) {
 
 		mediaRecorder = new MediaRecorder(mediaStream, {
 			mimeType: mediaRecorderCoder,
-			videoBitsPerSecond : 1000000 //1.0Gbps
+			videoBitsPerSecond : 1500000 //1.5Gbps
 		});
 		mediaRecorder.ondataavailable = function(e){
 			wsConnection.send(e.data);
 		}
-		mediaRecorder.start(3000); // Start recording, and dump data every 3 seconds
+		mediaRecorder.start(1000); // Start recording, and dump data every 3 seconds
 
 		try {
 			peerConnection.createOffer(offerOptions).then(gotDescription, errorHandler);
