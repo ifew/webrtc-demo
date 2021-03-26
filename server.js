adba1ad30eb2ef7db1147e14d5625109cb642e47
Chitpong Wuttanan <chitpong@orisma.com>
2021-03-26 15:27:25 +0700

    Changed to WSSocket

--- server.js
+++ server.js
@@ -2,16 +2,21 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-//const http = require('http');
-//const httpServer = http.Server(app);
 const https = require('https');
 const privateKey  = fs.readFileSync("server.key", 'utf8');
 const certificate = fs.readFileSync("server.crt", 'utf8');
 const credentials = {key: privateKey, cert: certificate};
-const httpsServer = https.createServer(credentials, app);
-const io = require('socket.io')(httpsServer);
+const WebSocketServer = require('ws').Server;
 const child_process = require('child_process');
 const port = 9000;
+
+const httpsServer = https.createServer(credentials, app).listen(port, () => {
+  console.log('Starting Socket.io Server on http://localhost:' + port);
+});
+
+const io = new WebSocketServer({
+  server: httpsServer
+});
 
 io.on('connection', function (client) {
   console.log('user connected');
@@ -30,103 +35,30 @@ io.on('connection', function (client) {
   // ffmpeg -f avfoundation -framerate 30 -pix_fmt uyvy422 -i "0" -s 1920x1080 -vcodec libx264 
   // -preset:v ultrafast -tune:v zerolatency -flvflags no_duration_filesize  -framerate 30 -vf showinfo 
   // -f flv rtmps://rtmp-global.cloud.vimeo.com:443/live/236536ce-0a7f-4e73-aed2-708620bbb8d3
-  //code sample from https://github.com/fbsamples/Canvas-Streaming-Example/blob/master/server.js
-  // const ffmpeg = child_process.spawn('ffmpeg', [
-  //   '-f', 'avfoundation', '-i', 'anullsrc',
-  //   '-framerate', '30',
-  //   '-i', '-',
-  //   '-s', '1920x1080',
-  //   '-vcodec', 'libx264',
-  //   '-preset:v', 'ultrafast',
-  //   '-tune:v', 'zerolatency',
-  //   '-flvflags', 'no_duration_filesize',
-  //   '-framerate', '30',
-  //   '-vf', 'showinfo',
-  //   '-shortest',
-  //   '-acodec', 'aac',
-  //   '-f', 'flv',
-  //   rtmpUrl
-  // ]);
   const ffmpeg = child_process.spawn('ffmpeg', [
-    // Facebook requires an audio track, so we create a silent one here.
-    // Remove this line, as well as `-shortest`, if you send audio from the browser.
     '-f', 'lavfi', '-i', 'anullsrc',
-
-    //added fix stdin send/receive
-    // '-r', '25',
-
-    // FFmpeg will read input video from STDIN
     '-i', '-',
-
-    //try to reduce low latency (normally 30sec)
-    //'-fflags', 'nobuffer', 
-    //'-probesize', '32', 
-    // '-preset', 'ultrafast', 
-    // '-s', '1920x1080',
-    // '-tune', 'zerolatency', 
-    // '-threads', '1',
-    
-    // Because we're using a generated audio source which never ends,
-    // specify that we'll stop at end of other input.  Remove this line if you
-    // send audio from the browser.
     '-shortest',
-    
-    // If we're encoding H.264 in-browser, we can set the video codec to 'copy'
-    // so that we don't waste any CPU and quality with unnecessary transcoding.
-    // If the browser doesn't support H.264, set the video codec to 'libx264'
-    // or similar to transcode it to H.264 here on the server.
-    // '-c:v','libvpx-vp9',
-    // '-c:a','libopus',
     '-vcodec', 'copy',
-    // '-vcodec', 'libx264',
-    
-    // AAC audio is required for Facebook Live.  No browser currently supports
-    // encoding AAC, so we must transcode the audio to AAC here on the server.
     '-acodec', 'aac',
-
-    //added fix stdin send/receive
-    // '-b:v', '1.5M', '-maxrate', '1.5M', '-bufsize', '1M',
-    // '-g', '90',
-    // '-movflags', '+faststart',
-    // '-pix_fmt', 'yuv420p',
-    // '-force_key_frames', '2',
-    //'-vf', 'yadif',
-    //'-crf', '18', '-bf', '2',
-    // '-r', '25', 
-    
-    // FLV is the container format used in conjunction with RTMP
-    // '-filter:v', 'fps=30',
     '-f', 'flv',
-    
-    // The output RTMP URL.
-    // For debugging, you could set this to a filename like 'test.flv', and play
-    // the resulting file with VLC.
     rtmpUrl,
-
-    //added fix stdin send/receive
-    //'-async', '1', '-vsync' ,'1'
   ]);
   
-  // If FFmpeg stops for any reason, close the WebSocket connection.
   ffmpeg.on('close', (code, signal) => {
     console.log('FFmpeg child process closed, code ' + code + ', signal ' + signal);
     client.terminate;
   });
-  
-  // Handle STDIN pipe errors by logging to the console.
-  // These errors most commonly occur when FFmpeg closes and there is still
-  // data to write.  If left unhandled, the server will crash.
+
   ffmpeg.stdin.on('error', (e) => {
     console.log('FFmpeg STDIN Error', e);
   });
   
-  // FFmpeg outputs all of its messages to STDERR.  Let's log them to the console.
   ffmpeg.stderr.on('data', (data) => {
     console.log('FFmpeg STDERR:', data.toString());
   });
 
   client.on('message', function (data) {
-    //console.log('message:' + data);
     ffmpeg.stdin.write(data);
   })
 
@@ -135,8 +67,4 @@ io.on('connection', function (client) {
   });
   
 
-});
-
-httpsServer.listen(port, () => {
-  console.log('Starting Socket.io Server on http://localhost:' + port);
 });
